8c92848496cec150081de3faa5af6395a52d2acc
MarynaPR <pryadkamaryna@gmail.com>
2021-09-20 11:18:07 -0500

    routes created

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,63 +1,174 @@
 from flask import Blueprint, request, jsonify, session
-from app.models import User
+from app.models import User, Post, Comment, Vote
 from app.db import get_db
-# facilitates error message display
+from app.utils.auth import login_required
+
+# error message display
 import sys
 
-# any routes defined within this file will automatically become part of Flask app with prefix of /api
 bp = Blueprint('api', __name__, url_prefix='/api')
 
 # POST route resolves to '/api/users' and uses the imported 'request' object which contains information about requests
 @bp.route('/users', methods=['POST'])
 def signup():
-  data = request.get_json()
-  db = get_db()
+    data = request.get_json()
+    db = get_db()
 
-  try:
-    newUser = User(
-      username=data['username'],
-      email=data['email'],
-      password=data['password']
-    )
+    try:
+        newUser = User(
+            username=data['username'],
+            email=data['email'],
+            password=data['password']
+        )
 
-    db.add(newUser)
-    db.commit()
-  except:
-    # should throw 'AssertionError' if custom validations fail and 'IntegrityError' with MySQL-related errors
-    print(sys.exc_info()[0])
-    # insert failed, so rollback (esures db will not lock up once deployed) and send error to front end
-    db.rollback()
-    return jsonify(message='Signup failed'), 500
+        db.add(newUser)
+        db.commit()
+    except:
+        # should throw 'AssertionError' if custom validations fail and 'IntegrityError' with MySQL-related errors
+        print(sys.exc_info()[0])
+        # insert failed, so rollback (esures db will not lock up once deployed) and send error to front end
+        db.rollback()
+        return jsonify(message='Signup failed'), 500
 
-  # clears any existing session data and creates 'user_id' session ppty to aid future db queries and boolean ppty that templates will use to conditionally render elements
-  session.clear()
-  session['user_id'] = newUser.id
-  session['loggedIn'] = True
+    # clears any existing session data and creates 'user_id' session ppty to aid future db queries and boolean ppty that templates will use to conditionally render elements
+    session.clear()
+    session['user_id'] = newUser.id
+    session['loggedIn'] = True
 
-  return jsonify(id=newUser.id)
+    return jsonify(id=newUser.id)
+
 
 @bp.route('/users/logout', methods=['POST'])
 def logout():
-  # remove session variables
-  session.clear()
-  return '', 204
+    # remove session variables
+    session.clear()
+    return '', 204
+
 
 @bp.route('/users/login', methods=['POST'])
 def login():
+    data = request.get_json()
+    db = get_db()
+
+    try:
+        user = db.query(User).filter(User.email == data['email']).one()
+    except:
+        print(sys.exc_info()[0])
+        return jsonify(message='Incorrect credentials'), 400
+
+    if user.verify_password(data['password']) == False:
+        return jsonify(message='Incorrect credentials'), 400
+
+    session.clear()
+    session['user_id'] = user.id
+    session['loggedIn'] = True
+
+    return jsonify(id=user.id)
+
+
+@bp.route('/comments', methods=['POST'])
+@login_required
+def comment():
+    data = request.get_json()
+    db = get_db()
+
+    try:
+        # create a new comment
+        newComment = Comment(
+            comment_text=data['comment_text'],
+            post_id=data['post_id'],
+            user_id=session.get('user_id')
+        )
+
+        db.add(newComment)
+        db.commit()
+    except:
+        print(sys.exc_info()[0])
+
+        db.rollback()
+        return jsonify(message='Comment failed'), 500
+
+    return jsonify(id=newComment.id)
+
+@bp.route('/posts/upvote', methods=['PUT'])
+@login_required
+def upvote():
   data = request.get_json()
   db = get_db()
 
   try:
-    user = db.query(User).filter(User.email == data['email']).one()
+    # create a new vote with incoming id and session id
+    newVote = Vote(
+      post_id = data['post_id'],
+      user_id = session.get('user_id')
+    )
+
+    db.add(newVote)
+    db.commit()
   except:
     print(sys.exc_info()[0])
-    return jsonify(message = 'Incorrect credentials'), 400
 
-  if user.verify_password(data['password']) == False:
-    return jsonify(message = 'Incorrect credentials'), 400
+    db.rollback()
+    return jsonify(message = 'Upvote failed'), 500
 
-  session.clear()
-  session['user_id'] = user.id
-  session['loggedIn'] = True
+  return '', 204
 
-  return jsonify(id = user.id)
+@bp.route('/posts', methods=['POST'])
+@login_required
+def create():
+  data = request.get_json()
+  db = get_db()
+
+  try:
+    # create a new post
+    newPost = Post(
+      title = data['title'],
+      post_url = data['post_url'],
+      user_id = session.get('user_id')
+    )
+
+    db.add(newPost)
+    db.commit()
+  except:
+    print(sys.exc_info()[0])
+
+    db.rollback()
+    return jsonify(message = 'Post failed'), 500
+
+  return jsonify(id = newPost.id)
+
+@bp.route('/posts/<id>', methods=['PUT'])
+@login_required
+def update(id):
+  data = request.get_json()
+  db = get_db()
+
+  try:
+    # retrieve post and update title property
+    post = db.query(Post).filter(Post.id == id).one()
+    post.title = data['title']
+    db.commit()
+  except:
+    print(sys.exc_info()[0])
+
+    db.rollback()
+    return jsonify(message = 'Post not found'), 404
+
+  return '', 204
+
+@bp.route('/posts/<id>', methods=['DELETE'])
+@login_required
+def delete(id):
+  db = get_db()
+
+  try:
+    # delete post from db
+    db.delete(db.query(Post).filter(Post.id == id).one())
+    db.commit()
+  except:
+    print(sys.exc_info()[0])
+
+    db.rollback()
+    return jsonify(message = 'Post not found'), 404
+
+  return '', 204
---
+++ app/utils/auth.py
@@ -0,0 +1,14 @@
+from flask import session, redirect
+from functools import wraps
+
+# define function login_requirements that receives another function as an argument
+def login_required(func):
+  @wraps(func)
+  def wrapped_function(*args, **kwargs):
+    # if logged in, call original function with original arguments
+    if session.get('loggedIn') == True:
+      return func(*args, **kwargs)
+
+    return redirect('/login')
+  
+  return wrapped_function
